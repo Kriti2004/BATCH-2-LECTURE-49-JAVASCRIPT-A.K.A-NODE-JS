5cfe0da43ab92862e9f514e49a104723ecdfafda
Kriti2004 <kritikiran20@gmail.com>
2023-06-26 15:28:03 +0530

    commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// I can write js code here
+
+console.log("Hi from External file");
+
+var aa = 1;
+
+var bb = 3;
---
+++ js/test.js
@@ -0,0 +1,23 @@
+
+// 1. variable Decteration 
+
+    var x;
+
+// 2. Variable Initialization
+
+    var y;
+    y = 20;
+
+// variable decleration and Initialization
+
+    var z = 40;
+
+    console.log(y+z);
+    
+    console.log('y = ', z);
+    console.log("Z ", z);
+
+    var e = 1;
+    //e = e +1;
+    e += e +1;
+    console.log("e =", e);
